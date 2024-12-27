afa0b54d9ad86942cd7a55944ed9b8115ef2c71a
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2024-12-27 18:03:41 +0100

    add passvowrd encoder

--- src/main/java/mk/ukim/finki/wp/lab/security/WebSecurityConfig.java
+++ src/main/java/mk/ukim/finki/wp/lab/security/WebSecurityConfig.java
@@ -10,12 +10,19 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public WebSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -48,11 +55,11 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails userAdmin =
                 User.builder()
                         .username("admin")
-                        .password("admin")
+                        .password(passwordEncoder.encode("admin"))
                         .roles("ADMIN")
                         .build();
         return new InMemoryUserDetailsManager(userAdmin);
